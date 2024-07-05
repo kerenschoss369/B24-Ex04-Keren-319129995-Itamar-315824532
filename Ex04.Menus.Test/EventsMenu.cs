8be1a5a2efe0035facce065eb012b91f87fc379f
kerenschoss <kerensc@mta.ac.il>
2024-07-05 16:44:59 +0300

    .

--- Ex04.Menus.Test/EventsMenu.cs
+++ Ex04.Menus.Test/EventsMenu.cs
@@ -9,36 +9,39 @@ namespace Ex04.Menus.Test
 {
     internal class EventsMenu
     {
-        public ExpandableMenuItem m_MainMenu; //should be MenuItem
-        ShowVersionListener m_ShowVersionListener = new ShowVersionListener();
-        CountCapitalsListener m_CountCapitalsListener = new CountCapitalsListener();
+        public ExpandableMenuItem m_MainMenu; 
         ShowDateListener m_ShowDateListener = new ShowDateListener();
         ShowTimeListener m_ShowTimeListener = new ShowTimeListener();
+        ShowVersionListener m_ShowVersionListener = new ShowVersionListener();
+        CountCapitalsListener m_CountCapitalsListener = new CountCapitalsListener();
 
         public EventsMenu()
         {
-            NonExpandableMenuItem showVersion = new NonExpandableMenuItem("Show Version");
-            NonExpandableMenuItem countCapitals = new NonExpandableMenuItem("Count Capitals");
             NonExpandableMenuItem showDate = new NonExpandableMenuItem("Show Time");
             NonExpandableMenuItem showTime = new NonExpandableMenuItem("Show Date");
-            ExpandableMenuItem showVersionOrCountCapitals = new ExpandableMenuItem("Version And Capitals");
+            NonExpandableMenuItem showVersion = new NonExpandableMenuItem("Show Version");
+            NonExpandableMenuItem countCapitals = new NonExpandableMenuItem("Count Capitals");
             ExpandableMenuItem showDateTime = new ExpandableMenuItem("Show Date/Time");
+            ExpandableMenuItem showVersionOrCountCapitals = new ExpandableMenuItem("Version And Capitals");
             ExpandableMenuItem eventsMenu = new ExpandableMenuItem("Events Main Menu");
+            
             //not sure about it for now
             NonExpandableMenuItem exit = new NonExpandableMenuItem("Exit");
             NonExpandableMenuItem back = new NonExpandableMenuItem("Back");
-            showVersion.Chosen += m_ShowVersionListener.ChosenAction;
-            countCapitals.Chosen += m_CountCapitalsListener.ChosenAction;
+
             showDate.Chosen += m_ShowDateListener.ChosenAction;
             showTime.Chosen += m_ShowTimeListener.ChosenAction;
-            showVersionOrCountCapitals.AddMenuItem(showVersion);
-            showVersionOrCountCapitals.AddMenuItem(countCapitals);
-            showVersionOrCountCapitals.AddMenuItem(back);// FIX
+            showVersion.Chosen += m_ShowVersionListener.ChosenAction;
+            countCapitals.Chosen += m_CountCapitalsListener.ChosenAction;
+
             showDateTime.AddMenuItem(showDate);
             showDateTime.AddMenuItem(showTime);
             showDateTime.AddMenuItem(back);// FIX
-            eventsMenu.AddMenuItem(showVersionOrCountCapitals);
+            showVersionOrCountCapitals.AddMenuItem(showVersion);
+            showVersionOrCountCapitals.AddMenuItem(countCapitals);
+            showVersionOrCountCapitals.AddMenuItem(back);// FIX
             eventsMenu.AddMenuItem(showDateTime);
+            eventsMenu.AddMenuItem(showVersionOrCountCapitals);
             eventsMenu.AddMenuItem(exit);// FIX
             m_MainMenu = eventsMenu;
         }
--- Ex04.Menus.Test/InterfaceMenu.cs
+++ Ex04.Menus.Test/InterfaceMenu.cs
@@ -10,38 +10,38 @@ namespace Ex04.Menus.Test
     internal class InterfaceMenu
     {
         public Ex04.Menus.Interfaces.ExpandableMenuItem m_MainMenu;
-        ShowVersionListener m_ShowVersionListener = new ShowVersionListener();
-        CountCapitalsListener m_CountCapitalsListener = new CountCapitalsListener();
         ShowDateListener m_ShowDateListener = new ShowDateListener();
         ShowTimeListener m_ShowTimeListener = new ShowTimeListener();
+        ShowVersionListener m_ShowVersionListener = new ShowVersionListener();
+        CountCapitalsListener m_CountCapitalsListener = new CountCapitalsListener();
 
         public InterfaceMenu()
         {
-            Ex04.Menus.Interfaces.NonExpandableMenuItem showVersion = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Show Version");
-            Ex04.Menus.Interfaces.NonExpandableMenuItem countCapitals = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Count Capitals");
             Ex04.Menus.Interfaces.NonExpandableMenuItem showDate = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Show Time");
             Ex04.Menus.Interfaces.NonExpandableMenuItem showTime = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Show Date");
-            Ex04.Menus.Interfaces.ExpandableMenuItem showVersionOrCountCapitals = new Ex04.Menus.Interfaces.ExpandableMenuItem("Version And Capitals");
+            Ex04.Menus.Interfaces.NonExpandableMenuItem showVersion = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Show Version");
+            Ex04.Menus.Interfaces.NonExpandableMenuItem countCapitals = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Count Capitals");
             Ex04.Menus.Interfaces.ExpandableMenuItem showDateTime = new Ex04.Menus.Interfaces.ExpandableMenuItem("Show Date/ Time");
+            Ex04.Menus.Interfaces.ExpandableMenuItem showVersionOrCountCapitals = new Ex04.Menus.Interfaces.ExpandableMenuItem("Version And Capitals");
             Ex04.Menus.Interfaces.ExpandableMenuItem interfaceMenu = new Ex04.Menus.Interfaces.ExpandableMenuItem("Interface Main Menu");
 
             //not sure about it for now
             Ex04.Menus.Interfaces.NonExpandableMenuItem exit = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Exit");
             Ex04.Menus.Interfaces.NonExpandableMenuItem back = new Ex04.Menus.Interfaces.NonExpandableMenuItem("Back");
 
-            showVersion.AddListener(m_ShowVersionListener);
-            countCapitals.AddListener(m_CountCapitalsListener);
             showDate.AddListener(m_ShowDateListener);
             showTime.AddListener(m_ShowTimeListener);
+            showVersion.AddListener(m_ShowVersionListener);
+            countCapitals.AddListener(m_CountCapitalsListener);
 
-            showVersionOrCountCapitals.AddMenuItem(showVersion);
-            showVersionOrCountCapitals.AddMenuItem(countCapitals);
-            showVersionOrCountCapitals.AddMenuItem(back);// FIX
             showDateTime.AddMenuItem(showDate);
             showDateTime.AddMenuItem(showTime);
             showDateTime.AddMenuItem(back);// FIX
-            interfaceMenu.AddMenuItem(showVersionOrCountCapitals);
+            showVersionOrCountCapitals.AddMenuItem(showVersion);
+            showVersionOrCountCapitals.AddMenuItem(countCapitals);
+            showVersionOrCountCapitals.AddMenuItem(back);// FIX
             interfaceMenu.AddMenuItem(showDateTime);
+            interfaceMenu.AddMenuItem(showVersionOrCountCapitals);
             interfaceMenu.AddMenuItem(exit);// FIX
             m_MainMenu = interfaceMenu;
         }
